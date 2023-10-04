f0676b75622ee0344a984a293c28b5bcb28b91d9
Anna Beatriz Bastos <127196700+Anna-hub19@users.noreply.github.com>
2023-10-04 10:17:12 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme:{
+        extend: {
+        colors: {
+            azul: {
+                claro: '#C5DFFF',
+                escuro: '#061E3C',
+                hover: '#1057B0'
+            }
+        },
+        fontFamily: {inter: ['Inter', 'sans-serif']}
+     }
+    }
+}
