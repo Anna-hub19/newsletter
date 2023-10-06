b6d3e8e1b3ac6a22e67e60948fe68ff577117402
anna-hub19 <annabastosbeatriz@gmail.com>
2023-10-06 10:09:33 -0300

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,26 @@
 tailwind.config = {
-    theme:{
+    theme: {
         extend: {
-        colors: {
-            azul: {
-                claro: '#C5DFFF',
-                escuro: '#061E3C',
-                hover: '#1057B0'
-            }
-        },
-        fontFamily: {inter: ['Inter', 'sans-serif']}
-     }
+            colors: {
+                azul: {
+                    claro: '#C5DFFF',
+                    escuro: '#061E3C',
+                    hover: '#1057B0'
+                }
+            },
+            fontFamily: { inter: ['Inter', 'sans-serif'] },
+
+            keyframes: {
+                sino_kf: {
+                    '0%, 100%': {
+                        transform: 'rotate(-10deg)'
+                    },
+                    '50%': { transform: 'rotate(10deg)' }
+
+                }
+            },
+            animation:{sino: 'sino_kf 0.31s ease-in-out infinite'}
+        }
+
     }
 }
